acd3d4bb01528c4b967457e8781efcd8cc59e3d1
Kazuki-Matsuo-AMS <k_matsuo@power.mech.waseda.ac.jp>
2019-11-08 23:42:16 +0900

    blockade_sign

--- darknet_ros/scripts/yolo_detection_manager.py
+++ darknet_ros/scripts/yolo_detection_manager.py
@@ -12,13 +12,22 @@ class YoloManager():
         rospy.init_node("yolo_manager")
         detect_target_subs = rospy.Subscriber("yolo/detect_target", Int8, self.detect_target_callback)
         found_object_subs = rospy.Subscriber("darknet_ros/bounding_boxes", BoundingBoxes,self.found_object_callback)
-        self.traffic_light_pub = rospy.Publisher("is_blue", Bool)
-        rospy.spin()
+        self.traffic_light_pub = rospy.Publisher("is_blue_traffic_light", Bool, queue_size=10)
+        self.blockade_sign_pub = rospy.Publisher("detect_blockade_sign", Bool, queue_size=10)
+	rate = rospy.Rate(5)
+	while not rospy.is_shutdown():
+	    pub_msg = Bool()
+            pub_msg.data = False
+            self.traffic_light_pub.publish(pub_msg)
+            self.blockade_sign_pub.publish(pub_msg)
+
+	    rate.sleep()
 
     def init_detecter(self):
         self.detect_light = "none"
-        self.list_size = 10;
+        self.list_size = 3;
         self.traffic_light_list = [0.] * self.list_size
+        self.blockade_sign_list = [0.] * self.list_size
 
     def detect_traffic_light(self, msg):
         if self.detect_light == "none":
@@ -31,6 +40,7 @@ class YoloManager():
                         self.init_detecter()
                         self.detect_light = "red"
                     break
+
         elif self.detect_light == "red":
             for obj in msg.bounding_boxes:
                 if obj.Class == "traffic_light_blue" and obj.probability > 0.7:
@@ -38,19 +48,29 @@ class YoloManager():
                     self.traffic_light_list.append(msg.header.stamp.secs + msg.header.stamp.nsecs * 1.0e-9)
                     dt = self.traffic_light_list[-1] - self.traffic_light_list[0]
                     if dt < 1:
-                        self.init_detecter()
-                        self.detect_light = "blue"
+			pub_msg = Bool()
+			pub_msg.data = True
+			self.traffic_light_pub.publish(pub_msg)
                     break
 
-
-        if self.detect_light == "blue":
-            pub_msg = Bool()
-            pub_msg.data = True
-            self.traffic_light_pub.publish(pub_msg)
+    def detect_blockade_sign(self, msg):
+	for obj in msg.bounding_boxes:
+	    #TODO add distance threshold
+	    if obj.Class == "blockade_sign" and obj.probability > 0.7:
+		self.blockade_sign_list.pop(0)
+		self.blockade_sign_list.append(msg.header.stamp.secs + msg.header.stamp.nsecs * 1.0e-9)
+		dt = self.blockade_sign_list[-1] - self.blockade_sign_list[0]
+		if dt < 1:
+		    pub_msg = Bool()
+		    pub_msg.data = True
+		    self.blockade_sign_pub.publish(pub_msg)
+		break
 
     def detect_target_callback(self, msg):
         if msg.data == 2:
             target_name = "traffic_light"
+	elif msg.data == 3:
+	    target_name = "blockade_sign"
         else:
             target_name = ""
 
@@ -71,6 +91,8 @@ class YoloManager():
     def found_object_callback(self,msg):
         if self.detect_target == "traffic_light":
             self.detect_traffic_light(msg)
+        if self.detect_target == "blockade_sign":
+            self.detect_blockade_sign(msg)
 
 if __name__ == "__main__":
     yolo_manager = YoloManager()
